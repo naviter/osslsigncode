8ab24afcd7cc8eafb5939e944a3f726bc49781eb
Simon Irmančnik <simon@naviter.com>
2025-04-09 13:58:24 +0200

    Reverting back to default engine for OpenSSL1.1.1 and default osslsigncode

--- sign.sh
+++ sign.sh
@@ -2,20 +2,25 @@
 
 # This script signs a Windows executable using osslsigncode with a YubiKey.
 # It creates a dual signature: SHA-1 for legacy compatibility and SHA-256 with RFC 3161 timestamp.
-# It uses OpenSSL3, OpenSC, connected YubiKey, exported certificate from YubiKey access and requires a patched version of osslsigncode.
+# It uses OpenSSL1.1.1, connected YubiKey and exported certificate from YubiKey.
 
 # Installation (https://www.pjrc.com/how-to-cross-compile-and-sign-windows-exe-on-linux-with-yubikey-token/)
 # sudo apt install yubikey-manager osslsigncode ykcs11 libengine-pkcs11-openssl opensc pcscd
 
-# Patched osslsigncode: https://github.com/naviter/osslsigncode.git
+# Patched osslsigncode (for OpenSSL3 & OpenSC): https://github.com/naviter/osslsigncode.git
 
 # Config
 NAME="NAVITER, d.o.o."
 URI="http://naviter.com"
 TIMESERVER="http://timestamp.digicert.com"
-PKCS11_MODULE="/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so"
+PKCS11_MODULE="/usr/lib/x86_64-linux-gnu/libykcs11.so"
 CERTS="./cert.pem"
-KEY="pkcs11:pin-value=xxxxxxxx"
+KEY="pkcs11:pin-value=xxxxxxxx" # Replace with your actual pin-value
+
+# Remove previous executable
+INPUT_TEMP="temp.exe"
+OUTPUT="signed.exe"
+rm -rf $INPUT_TEMP $OUTPUT
 
 # Input validation
 INPUT=$1
@@ -27,8 +32,6 @@ if [ ! -f "$INPUT" ]; then
   echo "Error: Input file '$INPUT' not found."
   exit 1
 fi
-INPUT_TEMP="temp.exe"
-OUTPUT="signed.exe"
 
 # Check if YubiKey is connected
 if ! opensc-tool -l | grep -q YubiKey; then
@@ -36,11 +39,8 @@ if ! opensc-tool -l | grep -q YubiKey; then
   exit 1
 fi
 
-# Remove previous executable
-rm -rf $INPUT_TEMP $OUTPUT
-
 # Step 1: SHA-1 signature (legacy compatibility)
-./osslsigncode sign \
+osslsigncode sign \
   -pkcs11module $PKCS11_MODULE \
   -certs $CERTS \
   -key $KEY \
@@ -52,7 +52,7 @@ rm -rf $INPUT_TEMP $OUTPUT
   -out $INPUT_TEMP || exit 1
 
 # Step 2: SHA-256 dual-signature with RFC 3161 timestamp
-./osslsigncode sign \
+osslsigncode sign \
   -pkcs11module $PKCS11_MODULE \
   -certs $CERTS \
   -key $KEY \
